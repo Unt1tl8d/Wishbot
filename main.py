0a1195ef2b0c090a59ec13cf4db5ecb6839b2915
Unt1tl8d <kent.spike24x7@gmail.com>
2024-12-05 01:16:14 +0300

    3

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__, template_folder='html')
 def web():  
     return render_template('index.html')  
   
-if __name__ == "__main__":  
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port='80')
